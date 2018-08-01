25422812ae3b09208a98a0d07b0cc7e5f4196ee7
urosjevremovic <jevremovic.uros91@gmail.com>
2018-08-01 21:48:20 +0200

    added docs

--- Gecko_web_app_API/urls.py
+++ Gecko_web_app_API/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+API_TITLE = 'GitHub API web app'
+schema_view = get_schema_view(title=API_TITLE)
+API_DESCRIPTION = 'A GitHub API based web app.'
 
 from Gecko_web_app_API import settings
 from repository.views import RepositoryViewSet
@@ -29,7 +35,8 @@ router.register('repository', RepositoryViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/rest-auth/', include('rest_auth.urls')),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path('schema/', schema_view),
 ]
 
 urlpatterns += router.urls
